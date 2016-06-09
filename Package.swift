6a0ae0a29016faac2ebaea71a0daecc06795d149
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-10 06:28:32 +1000

    Add gtk-3.18 (glib-2.46) dependency

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftGtkApp"
+    name: "SwiftGtkApp",
+    dependencies: [
+        .Package(url: "https://github.com/rhx/SwiftGtk.git", Version("3.18.0-2.46")!)
+    ]
 )
