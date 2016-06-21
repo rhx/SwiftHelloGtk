bc6a51376b7a339f5fc5f6689a2aa09a67d11d3a
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-21 17:33:22 +1000

    Simplify the app using the static run() method

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,14 +1,15 @@
 import Gtk
 
-let app = Application()!
-
-let status = app.run {
+let status = Application.run { app in
     var window = ApplicationWindowRef(application: app)
     window.title = "Hello, world"
     window.setDefaultSize(width: 640, height: 480)
     window.showAll()
 }
 
+guard let status = status else {
+    fatalError("Could not create Application")
+}
 guard status == 0 else {
     fatalError("Application exited with status \(status)")
 }
