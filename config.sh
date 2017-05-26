f240eca7c1302a7e21f6e5b42d33bb4b6cb1038c
Rene Hexel <r.hexel@griffith.edu.au>
2017-05-27 08:19:29 +1000

    Remove '-Wl,' from linker flags

--- config.sh
+++ config.sh
@@ -16,7 +16,7 @@ BUILD_BIN=${BUILD_DIR}/debug
 BUILT_PRODUCTS_DIR=${BUILD_DIR}/app
 export PATH="${BUILD_DIR}/gir2swift/.build/release:${BUILD_DIR}/gir2swift/.build/debug:${PATH}"
 GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
-LINKFLAGS=`pkg-config --libs gtk+-$ver gdk-$ver pangocairo pangoft2 pango gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`" -Xlinker -L/usr/local/lib"
+LINKFLAGS=`pkg-config --libs gtk+-$ver gdk-$ver pangocairo pangoft2 pango gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' '`" -Xlinker -L/usr/local/lib"
 CCFLAGS=`pkg-config --cflags gtk+-$ver gdk-$ver pangocairo pangoft2 pango gobject-2.0 gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
 TAC="tail -r"
 if which tac >/dev/null ; then
