bff815341333c85fe541dda53dc35d585dca1a24
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-04 09:41:19 +1000

    Use gtk3 branch

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HelloGtk",
     dependencies: [
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("main")),
+        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
     ],
     targets: [
         .target(name: "HelloGtk", dependencies: ["Gtk"]),
