39569a718cce7fb0e6f7b7c73141826a8be4d554
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-11 10:39:23 +1000

    Introduce new build system

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "HelloGtk",
     dependencies: [
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("development")),
+        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("development")),
     ],
     targets: [
         .target(name: "HelloGtk", dependencies: ["Gtk"]),
