8362ce7e36ad149f2d770f79459d0fed6ee03b1c
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-13 14:21:33 +1000

    Add gir2swift dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "HelloGtk",
     dependencies: [
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
         .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
     ],
     targets: [
