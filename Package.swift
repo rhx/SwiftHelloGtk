538c70b7f2107b67b4cd8cfdb0bb75ab91d671e8
Rene Hexel <r.hexel@griffith.edu.au>
2023-07-29 15:59:02 +1000

    Modernise Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "HelloGtk",
     dependencies: [
-        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("monorepo")),
+        .package(url: "https://github.com/rhx/gir2swift.git", branch: "main"),
+        .package(url: "https://github.com/rhx/SwiftGtk.git",  branch: "monorepo"),
     ],
     targets: [
         .target(name: "HelloGtk", dependencies: [
